6c3a8afd653d334f23c8bf2aa0b032c17aac40fe
Harshit Bhatt <harshitbhatt0007@gmail.com>
2021-04-28 20:06:28 +0530

    Refactoring and removed node modules

--- app.js
+++ app.js
@@ -1,79 +1,80 @@
 var express = require("express"),
 	app = express(),
-	bodyparser=require("body-parser"),
-	passport=require("passport"),
-	localstrategy=require("passport-local"),
-	mongoose=require("mongoose"),
+	bodyparser = require("body-parser"),
+	passport = require("passport"),
+	localstrategy = require("passport-local"),
+	mongoose = require("mongoose"),
 	passportLocalMongoose = require("passport-local-mongoose"),
 	googleStrategy = require("passport-google-oauth20"),
-	keys = require("./keys"),
-	methodOverride = require("method-override"),
-	var fs = require("fs");
+	methodOverride = require("method-override")
+var fs = require("fs");
 
-	var states = JSON.parse(fs.readFileSync("states.json").toString());
+require('dotenv').config()
+
+var states = JSON.parse(fs.readFileSync("states.json").toString());
 
 app.use(methodOverride("_method"));
 
 var authRoutes = require("./routes/auth-routes"),
-		profileRoutes = require("./routes/profile-routes"),
-		restaurantRoutes = require("./routes/restaurant-routes");
+	profileRoutes = require("./routes/profile-routes"),
+	restaurantRoutes = require("./routes/restaurant-routes");
 
 var customer = require("./models/customer");
 var restaurant = require("./models/restaurant");
 
-app.listen(8080, function(){
+app.listen(8080, function () {
 	console.log("Listening on port: " + this.address().port);
 });
 
 app.use(require("express-session")({
-    secret: "VIT",
-    resave: false,
-    saveUninitialized: false
+	secret: "VIT",
+	resave: false,
+	saveUninitialized: false
 }));
 
 app.use(passport.initialize());
 app.use(passport.session());
 
 passport.use(
-		new googleStrategy({
-				callbackURL: '/auth/google/redirect',
-				clientID: keys.google.clientID,
-				clientSecret: keys.google.clientSecret
-			}, function(accessToken, refreshToken, profile, done){
+	new googleStrategy({
+		callbackURL: '/auth/google/redirect',
+		clientID: process.env.GOOGLE_CLIENT_ID,
+		clientSecret: process.env.GOOGLE_CLIENT_SECRET
+	}, function (accessToken, refreshToken, profile, done) {
 
 		// check if user already exist
-						customer.findOne({googleid: profile.id}, function(err, data){
-								if(err){
-								console.log(err);
-								} else if(data){
-										app.locals.new = false;
-										done(null, data);
-									} else {
-											new customer({
-													googleid: profile.id,
-													name: profile.displayName,
-													email: profile.emails[0].value,
-													address: "",
-													state: "",
-													district: "",
-													pincode: "",
-													contact: "",
-													restaurant: "",
-													orderaccepted: 0,
-													foodsent: ""
-											}).save().then(function(newUser){
-													app.locals.new = true;
-													done(null, newUser);
-												});
-										}
-						});
-		})
+		customer.findOne({ googleid: profile.id }, function (err, data) {
+			if (err) {
+				console.log(err);
+			} else if (data) {
+				app.locals.new = false;
+				done(null, data);
+			} else {
+				new customer({
+					googleid: profile.id,
+					name: profile.displayName,
+					email: profile.emails[0].value,
+					address: "",
+					state: "",
+					district: "",
+					pincode: "",
+					contact: "",
+					restaurant: "",
+					orderaccepted: 0,
+					foodsent: ""
+				}).save().then(function (newUser) {
+					app.locals.new = true;
+					done(null, newUser);
+				});
+			}
+		});
+	})
 );
 
 passport.use("restaurant-local", new localstrategy(restaurant.authenticate()));
 
-mongoose.connect("mongodb://localhost:27017/foodDelivery",{useNewUrlParser: true,});
-app.use(bodyparser.urlencoded({extended: true}));
+mongoose.connect("mongodb://localhost:27017/foodDelivery", { useNewUrlParser: true, });
+app.use(bodyparser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
 app.use('/auth', authRoutes);
@@ -81,42 +82,42 @@ app.use('/profile', profileRoutes);
 app.use('/restaurant', restaurantRoutes);
 
 // serializing user
-passport.serializeUser(function(id, done){
-	customer.findById(id).then(function(user){
-		if(user){
-				done(null, user);
+passport.serializeUser(function (id, done) {
+	customer.findById(id).then(function (user) {
+		if (user) {
+			done(null, user);
 		} else {
-				restaurant.findById(id).then(function(user){
-					done(null, user);
-				});
-			}
-	});
-});
-
-// deserializing user
-passport.deserializeUser(function(id, done){
-	customer.findById(id).then(function(user){
-		if(user){
+			restaurant.findById(id).then(function (user) {
 				done(null, user);
-		} else {
-				restaurant.findById(id).then(function(user){
-					done(null, user);
-				});
+			});
 		}
 	});
 });
 
-app.get("/", function(req, res){
-		res.render("home", {state: states});
+// deserializing user
+passport.deserializeUser(function (id, done) {
+	customer.findById(id).then(function (user) {
+		if (user) {
+			done(null, user);
+		} else {
+			restaurant.findById(id).then(function (user) {
+				done(null, user);
+			});
+		}
+	});
 });
 
-app.get("/search/home", function(req, res){
-	var searchedCity = req.query.city.charAt(0).toUpperCase()+req.query.city.substring(1);
-		restaurant.find({city: searchedCity}, function(err, restaurants){
-				if(err){
-					console.log(err);
-				} else {
-					res.render("landing",{customer: req.user, location: searchedCity, restaurant: restaurants});
-				}
-		});
+app.get("/", function (req, res) {
+	res.render("home", { state: states });
+});
+
+app.get("/search/home", function (req, res) {
+	var searchedCity = req.query.city.charAt(0).toUpperCase() + req.query.city.substring(1);
+	restaurant.find({ city: searchedCity }, function (err, restaurants) {
+		if (err) {
+			console.log(err);
+		} else {
+			res.render("landing", { customer: req.user, location: searchedCity, restaurant: restaurants });
+		}
+	});
 });
--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -1,26 +1,25 @@
 var router = require("express").Router();
 var passport = require("passport");
 var customer = require("../models/customer");
-var messagebird = require("messagebird")("IaxoJGl8RXuxJI6M3BkAJql7a");
 
-router.get("/login", function(req, res){
+router.get("/login", function (req, res) {
 	res.render("customerlogin");
 });
 
-router.get("/logout", function(req, res){
+router.get("/logout", function (req, res) {
 	res.send("Logging out");
 });
 
 router.get("/google", passport.authenticate("google", {
-	scope: ["profile","email"]
+	scope: ["profile", "email"]
 }));
 
-router.get('/google/redirect', passport.authenticate("google"), function(req, res){
-	if(req.app.locals.new){
+router.get('/google/redirect', passport.authenticate("google"), function (req, res) {
+	if (req.app.locals.new) {
 		res.redirect("/profile/register");
 	} else {
-		customer.findById(req.user._id, function(err, object){
-			if(err){
+		customer.findById(req.user._id, function (err, object) {
+			if (err) {
 				console.log(err);
 			} else {
 				res.redirect("/profile/" + object._id);
@@ -28,30 +27,5 @@ router.get('/google/redirect', passport.authenticate("google"), function(req, re
 		});
 	}
 });
-
-// router.post("/mobile", function(req, res){
-// 		var mobile = req.body.mobile;
-// 		messagebird.verify.create(mobile, {template: "Your Verification Code for Foodyz is %token"}, function(err, response){
-// 				if(err){
-// 						console.log(err);
-// 				} else {
-// 						console.log(response);
-// 						res.render("mobileverification", {id: response.id});
-// 				}
-// 		});
-// });
-//
-// router.post("/mobile/codeverification", function(req, res){
-// 		var id = req.body.id,
-// 				token = req.body.token;
-// 		messagebird.verify.verify(id, token, function(err, response){
-// 				if(err){
-// 						console.log(err);
-// 						res.redirect("/auth/login");
-// 				} else {
-// 						res.send("Succcesfully verified the customer");
-// 				}
-// 		});
-// });
 
 module.exports = router;
